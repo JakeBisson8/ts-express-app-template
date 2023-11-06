f30d0b75051048bfe59f2c5a08b413edfd556eb1
Jake Bisson <Jake.Bisson@nrc-cnrc.gc.ca>
2023-11-06 15:32:11 -0500

    Add helmet middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import helmet from 'helmet';
 
 require('dotenv-safe').config();
 
 const app = express();
+
+app.use(helmet());
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World!' });
