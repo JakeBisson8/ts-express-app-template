9f2a50a2893d1de7b26146a846e66bc281a9c5ca
Jake Bisson <Jake.Bisson@nrc-cnrc.gc.ca>
2023-11-06 17:30:33 -0500

    Updated morgan to log to files

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
 
 require('dotenv-safe').config();
 
@@ -9,10 +11,30 @@ const app = express();
 
 app.use(helmet());
 app.use(cors());
+
+// create log directory
+const directory = path.join(__dirname, 'logs/');
+if (!fs.existsSync(directory)) {
+  fs.mkdirSync(directory);
+}
+
+// Morgan setup
+const httpLogCombinedStream = fs.createWriteStream(path.join(__dirname, 'logs/httprequests_combined.log'), {
+  flags: 'a',
+});
+const httpErrorLogStream = fs.createWriteStream(path.join(__dirname, 'logs/httprequests_error.log'), { flags: 'a' });
+app.use(morgan('common', { stream: httpLogCombinedStream }));
+app.use(
+  morgan('common', {
+    skip: (req, res) => res.statusCode < 400,
+    stream: httpErrorLogStream,
+  }),
+);
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
 
+// Default endpoint
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World!' });
 });
